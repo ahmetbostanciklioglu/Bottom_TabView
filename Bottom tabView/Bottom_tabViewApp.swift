5d4cb6315a3a94a9c9f0db209eb44bfc92c40b00
Ahmet <ahmetbostancikli@gmail.com>
2025-12-03 10:56:37 +0300

    Initial Commit

---
+++ Bottom tabView/Bottom_tabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bottom_tabViewApp.swift
+//  Bottom tabView
+//
+//  Created by MrBostan on 3.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Bottom_tabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
